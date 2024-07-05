96be0b8fe6892d586fbc7c2327be0f634fa34b86
Juan Ferraro <135638564+DominusJP@users.noreply.github.com>
2024-07-05 00:16:43 -0300

    Update 6-number_odd_or_even.py

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -1,57 +1,57 @@
 #!/usr/bin/python3
-
-"""Script that starts a Flask web application"""
-
+"""
+A simple flask server running on 0.0.0.0:5000
+"""
 from flask import Flask, render_template
+
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
-@app.route("/", strict_slashes=False)
-def hello_route():
-    """app displaying Hello"""
-
+@app.route('/')
+def display_root():
+    """Prints 'Hello HBNB!' to display"""
     return "Hello HBNB!"
 
 
-@app.route("/hbnb", strict_slashes=False)
-def hello_route_v1():
-    """App displaying 2 cmd"""
-
+@app.route('/hbnb')
+def display_hbnb():
+    """Prints 'HBNB' to display"""
     return "HBNB"
 
 
 @app.route("/c/<text>", strict_slashes=False)
-def hello_route_v2(text):
-    """App displaying 3 cmd"""
-
-    return 'C ' + text.replace('_', ' ')
-
-
-@app.route("/python", strict_slashes=False)
-@app.route("/python/<text>", strict_slashes=False)
-def hello_route_v3(text='is cool'):
-    """App displaying 4 cmd"""
-
-    return 'Python ' + text.replace('_', ' ')
+def display_c(text):
+    """Displays 'C' followed by the value of <text>."""
+    text = text.replace("_", " ")
+    return "C {}".format(text)
 
 
-@app.route("/number/<int:n>", strict_slashes=False)
-def hello_route_v4(n):
-    """App displaying 4 cmd"""
+@app.route('/python/')
+@app.route('/python/<text>')
+def display_python(text="is cool"):
+    """Print route param to display"""
+    text = text.replace("_", " ")
+    return "Python {}".format(text)
+
+
+@app.route('/number/<int:n>')
+def display_n(n):
+    """Print n if n is int"""
     return "{} is a number".format(n)
 
 
-@app.route("/number_template/<int:n>", strict_slashes=False)
-def hello_route_v5(n):
-    """App displaying 6 cmd"""
+@app.route('/number_template/<int:n>')
+def display_n_template(n):
+    """Return an html page with value of n"""
     return render_template('5-number.html', n=n)
 
 
-@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
-def hello_route_v6(n):
-    """App displaying 7 cmd"""
+@app.route('/number_odd_or_even/<int:n>')
+def diplay_n_odd_or_even(n):
+    """Return html page with if n is odd or even"""
     return render_template('6-number_odd_or_even.html', n=n)
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
