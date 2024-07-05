9f4a17ba5d2d76615784ecffaea07d0d29e67d6a
Juan Ferraro <135638564+DominusJP@users.noreply.github.com>
2024-07-05 00:58:18 -0300

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,62 +8,41 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
+
     def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        if cls is None:
-            return self.__objects
-        cls_dict = {}
-        for key, val in FileStorage.__objects.items():
-            if type(val) == cls:
-                cls_dict[key] = val
-        return cls_dict
+        if cls is not None:
+            class_dict = {}
+            for key, value in FileStorage.__objects.items():
+                if type(value) is cls:
+                    class_dict[key] = value
+
+            return class_dict
+        else:
+            return FileStorage.__objects
+
+    def delete(self, obj=None):
+        """ Public instance method that deletes given object"""
+        try:
+            key = "{}.{}".format(type(obj).__name__, obj.id)
+            del FileStorage.__objects[key]
+        except Exception:
+            pass
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
-        self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
-
-    def save(self):
-        """Saves storage dictionary to file"""
-        with open(FileStorage.__file_path, 'w') as f:
-            temp = {}
+	@@ -39,7 +29,7 @@ def save(self):
             temp.update(FileStorage.__objects)
             for key, val in temp.items():
                 temp[key] = val.to_dict()
-            json.dump(temp, f, indent=4)
+            json.dump(temp, f)
 
     def reload(self):
         """Loads storage dictionary from file"""
-        from models.base_model import BaseModel
-        from models.user import User
-        from models.place import Place
-        from models.state import State
-        from models.city import City
-        from models.amenity import Amenity
-        from models.review import Review
-
-        classes = {
-                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
-                    'State': State, 'City': City, 'Amenity': Amenity,
-                    'Review': Review
-                  }
-        try:
-            temp = {}
-            with open(FileStorage.__file_path, 'r') as f:
-                temp = json.load(f)
-                for key, val in temp.items():
-                    self.all()[key] = classes[val['__class__']](**val)
+	@@ -65,6 +55,15 @@ def reload(self):
         except FileNotFoundError:
             pass
 
-    def delete(self, obj=None):
-        """ Delete obj from __objects """
-        fs = FileStorage()
-        if obj is None:
-            return
-        key = obj.__class__.__name__ + "." + obj.id
-        if key in fs.__objects:
-            del fs.__objects[key]
-
     def close(self):
-        """ jasjejeja sdjsejae ksdkdf s s """
+        """Seyyarin can sagligi"""
         self.reload()
