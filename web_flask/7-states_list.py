9bdcbf21aea6c19af6d50c212511de7ba387b6e1
Juan Ferraro <135638564+DominusJP@users.noreply.github.com>
2024-07-05 01:39:40 -0300

    Update 7-states_list.py

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,24 +1,23 @@
 #!/usr/bin/python3
-"""import flask and models"""
-from flask import Flask, render_template
-from models import storage
-from models.state import State
+''' Flask web application '''
 
+from flask import Flask
+from flask import render_template
+from models.state import State
+from models import storage
 
 app = Flask(__name__)
 
 
 @app.route('/states_list', strict_slashes=False)
-def display_states():
-    """variable show the list"""
-    state = sorted(storage.all(State).values(), key=lambda x: x.name)
-    return render_template("7-states_list.html", states=state)
+def state_list():
+    all_states = storage.all(State)
+    return render_template('7-states_list.html', states=all_states)
 
 
 @app.teardown_appcontext
-def close_db(error):
-    """close"""
-    storage.close()
+def teardown_appcontext(self):
+    return storage.close()
 
 
 if __name__ == '__main__':
