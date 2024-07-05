c7e1196221b45635c6f5193fb09a87f54e05ad2e
Juan Ferraro <135638564+DominusJP@users.noreply.github.com>
2024-07-05 00:56:36 -0300

    Update state.py

--- models/state.py
+++ models/state.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from os import getenv
 from models.city import City
@@ -9,21 +9,22 @@ from models.city import City
 
 class State(BaseModel, Base):
     """ State class """
-    __tablename__ = "states"
+    __tablename__ = 'states'
     name = Column(String(128), nullable=False)
+    cities = relationship('City', cascade='all, delete', backref='state')
 
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        cities = relationship(
-            "City",
-            backref="state",
-            cascade='all, delete-orphan')
-    else:
+    def __init__(self, *args, **kwargs):
+        """ Initialization of State instance """
+        super().__init__(*args, **kwargs)
+
+    if getenv('HBNB_TYPE_STORAGE') != 'db':
         @property
         def cities(self):
+            """Getter Function for FileStorage"""
             from models import storage
-            c_instances = storage.all(City).values()
-            m_cities = []
-            for city in c_instances:
+            result = []
+            all_cities = storage.all(City)
+            for city in all_cities.values():
                 if city.state_id == self.id:
-                    m_cities.append(city)
-            return m_cities
+                    result.append(city)
+            return result
