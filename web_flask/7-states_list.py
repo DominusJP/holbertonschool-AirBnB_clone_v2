e1a1de2102a7d807eed4cfbb75a7306ed2221133
Juan Ferraro <135638564+DominusJP@users.noreply.github.com>
2024-07-05 00:38:01 -0300

    Update 7-states_list.py

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,33 +1,25 @@
 #!/usr/bin/python3
-"""
-Simple module that starts a Flask web application
-Starting to display formatted text and conditional message
-Addind basic template
-"""
+"""Python script that starts a Flask web application"""
+
+
 from flask import Flask, render_template
 from models import storage
 from models.state import State
 
+
 app = Flask(__name__)
 
 
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    depo = storage.all(State).values()
+    return render_template("7-states_list.html", depo=depo)
+
+
 @app.teardown_appcontext
-def handle_appcontext(self):
-    """
-    Method for handling app context
-    """
+def teardown(exc):
     storage.close()
 
 
-@app.route('/states_list', strict_slashes=False)
-def list_states():
-    """
-    Displays 7-states_list html page
-    """
-    states = storage.all(State).values()
-    return render_template('7-states_list.html', states=states)
-
-
 if __name__ == "__main__":
-    """ app listening on host 0.0.0.0 and port 5000 """
-    app.run(host='0.0.0.0', port='5000')
+    app.run(host="0.0.0.0", port=5000)
