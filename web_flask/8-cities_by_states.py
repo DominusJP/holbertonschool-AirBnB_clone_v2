dde97e72f160bc4ad4ff3b4298b0b4557c12fa00
Juan Ferraro <135638564+DominusJP@users.noreply.github.com>
2024-07-05 00:24:17 -0300

    Update 8-cities_by_states.py

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,24 +1,27 @@
 #!/usr/bin/python3
-"""import"""
+"""
+A flask server running on 0.0.0.0:5000
+"""
 from flask import Flask, render_template
 from models import storage
 from models.state import State
 
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
-def teardown(exception):
-    """close"""
+def remove_current_session(exc):
+    """Remove the current SQLAlchemy session."""
     storage.close()
 
 
-@app.route("/cities_by_states", strict_slashes=False)
-def cities_by_states():
-    """Displays cities by states"""
+@app.route('/cities_by_states', strict_slashes=False)
+def display_states():
+    """Prints html document with a list of states"""
     states = storage.all(State).values()
-    return render_template("8-cities_by_states.html", states=states)
+    return render_template('8-cities_by_states.html', states=states)
 
 
 if __name__ == "__main__":
